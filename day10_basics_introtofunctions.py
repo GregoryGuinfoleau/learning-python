8375d181e80b5c455cfadbf94aa7c8bb7f58d2aa
GregoryGuinfoleau <gregory_guinfoleau@bruxxel.org>
2026-04-24 10:39:49 -0500

    Day 10 : Intro. to functions

---
+++ day10_basics_introtofunctions.py
@@ -0,0 +1,57 @@
+# Day 10 - Codecademy Python 3
+# [Intro to functions]
+# Date: April 23, 2026
+
+# ======================
+# [SECTION 1 - Premier concept appris]
+# ======================
+
+# Functions are a convenient way to group our code into 
+# reusable blocks. A function contains a sequence of steps 
+# that can be performed repeatedly throughout a program without
+# having to repeat the process of writing the same code again
+
+# Pour écrire une fonction :
+
+# def nom_fonction():
+#   début du code
+
+# Exemple :
+def welcome():
+    print("Bienvenue!")
+
+# Ceci n'affichera pas Bienvenue, il faut "appeler" la fonction pour cela.
+# tout simplement en écrivant nom_fonction() après l'avoir défini bine sûr !
+
+welcome()
+
+
+# ======================
+# [SECTION 2 - Deuxième concept]
+# ======================
+
+# Explication
+# Exemple :
+
+
+# ======================
+# [SECTION 3 - etc.]
+# ======================
+
+# Explication
+# Exemple :
+
+# ======================
+# CE QUE J'AI COMPRIS
+# ======================
+
+# - Point clé 1 retenu
+# - Point clé 2
+# - Point clé 3
+# - Ce qui était difficile / ce que je veux revoir
+
+# ======================
+# EXERCICE PRATIQUE (optionnel)
+# ======================
+
+# Si je veux tester ce que j'ai appris avec un exercice perso
